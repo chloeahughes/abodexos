0c4576a72d26934bc459af862a46f012e50fb09b
Chloe Hughes <cahughes@stanford.edu>
2025-09-26 19:22:28 -0700

    add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ✅ Don’t fail the build on ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
